201129f34ee05d7bb65f09598b5f90e726820578
Patrick23718 <32969033+Patrick23718@users.noreply.github.com>
2021-11-12 11:40:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,19 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyparser.json());
 app.use("/uploads", express.static("uploads"));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 // app.use(cookieParser());
 // app.use(csrfMiddleware);
 
